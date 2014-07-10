51837ad2de4b93e158cced2211172094ecb40da4
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-07-10 18:56:29 +0900

    rename from DoorkeeperAuthServerExample to DoorkeeperProviderExample

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_doorkeeper-auth-server-example_session'
+Rails.application.config.session_store :cookie_store, key: '_doorkeeper-provider-app_session'
