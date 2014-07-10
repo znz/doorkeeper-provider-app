d893e2eb9a2f9ff5d59195dec4e17835678c72c0
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-07-10 16:07:25 +0900

    do not generate unused files

---
+++ config/initializers/generators.rb
@@ -0,0 +1,8 @@
+Rails.application.config.generators do |g|
+  g.assets       false # javascripts and stylesheets
+  g.helper       false
+  g.helper_specs false
+  g.javascripts  false
+  g.stylesheets  false
+  g.view_specs   false
+end
