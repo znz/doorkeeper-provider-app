80d841368f01e768dad46321a3b8ed4cb8894ab0
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-07-10 15:51:04 +0900

    sign_in user before GET

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe WelcomeController, :type => :controller do
+RSpec.describe WelcomeController, type: :controller do
+  let(:user) { FactoryGirl.create(:user) }
 
   describe "GET 'index'" do
     it "returns http success" do
+      sign_in user
       get 'index'
       expect(response).to be_success
     end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,7 @@
 
 FactoryGirl.define do
   factory :user do
+    sequence(:email)    { |n| "user#{n}@example.com" }
+    sequence(:password) { |n| "password#{n}" }
   end
 end
