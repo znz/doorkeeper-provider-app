dbbe33b09dba7c8a05f1cdfd0c512fe13632e923
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-07-10 15:51:27 +0900

    add root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
   use_doorkeeper
   devise_for :users
 
@@ -8,7 +6,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
