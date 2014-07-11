b2ae147708a8ffc099180fb6135b215d6f6e5d5f
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-07-11 14:07:14 +0900

    pending reqest spec

--- spec/requests/microposts_spec.rb
+++ spec/requests/microposts_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "Microposts", :type => :request do
+RSpec.describe "Microposts", type: :request do
   describe "GET /microposts" do
-    it "works! (now write some real specs)" do
+    pending "works! (now write some real specs)" do
       get microposts_path
       expect(response.status).to be(200)
     end
