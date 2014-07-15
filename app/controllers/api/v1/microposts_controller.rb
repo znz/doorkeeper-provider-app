e2c0116484afeddcecba26c044dacc760c28fa6c
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-07-15 18:29:44 +0900

    use skip_before_action instead of skip_before_filter

--- app/controllers/api/v1/microposts_controller.rb
+++ app/controllers/api/v1/microposts_controller.rb
@@ -5,7 +5,7 @@ module Api::V1
     respond_to     :json
 
     # avoid "Can't verify CSRF token authenticity"
-    skip_before_filter :verify_authenticity_token
+    skip_before_action :verify_authenticity_token
 
     def index
       @microposts = Micropost.all
