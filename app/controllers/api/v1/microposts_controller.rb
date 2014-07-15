9f1a86d288d3486e011b535971ef6df581913927
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-07-15 10:46:13 +0900

    avoid "Can't verify CSRF token authenticity" via API

--- app/controllers/api/v1/microposts_controller.rb
+++ app/controllers/api/v1/microposts_controller.rb
@@ -4,6 +4,9 @@ module Api::V1
     doorkeeper_for :create, :update, scopes: %w"admin write"
     respond_to     :json
 
+    # avoid "Can't verify CSRF token authenticity"
+    skip_before_filter :verify_authenticity_token
+
     def index
       @microposts = Micropost.all
       respond_with @microposts
